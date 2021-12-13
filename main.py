0d573b57eb9b416db8bf6bbfb294d3bdda471909
macksjeremy <31860185+macksjeremy@users.noreply.github.com>
2021-12-13 16:08:52 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+#Used for graphing
+import matplotlib.pyplot as plt
+
+N = 10000
+I = 0
+
+
+outcirclex = []
+outcircley = []
+incirclex = []
+incircley = []
+
+for i in range(N):
+    #initialize point balue
+    x = random.random()
+    y = random.random()
+    point = (x,y)
+    #Check if the item hits the dartboard or not.
+    if((.5-x)**2 + (.5 - y)** 2) < .25:
+        I += 1
+        incirclex.append(x)
+        incircley.append(y)
+    else:
+        outcirclex.append(x)
+        outcircley.append(y)
+
+#Calculate pi.
+pi = 4 * I/N
+print(pi)
+for i in range(len(incirclex)):
+    plt.plot(incirclex[i],incircley[i], 'rx')
+for i in range(len(outcircley)):
+    plt.plot(outcirclex[i],outcircley[i], 'bo')
+plt.show()
+
