303da18442a2c0a54370848458bb581b6b55fa9d
macksjeremy <31860185+macksjeremy@users.noreply.github.com>
2022-02-01 15:05:13 -0500

    Realized my LISS was naive :/

--- DP/LongestIncreasingSubsequence.py
+++ DP/LongestIncreasingSubsequence.py
@@ -1,17 +1,17 @@
 #Assuming all values in arr are positive.
 #To fix that, change 0 to -999
-def lissubproblem(arr, i, prev):
+def UNOPTIMALlissubproblem(arr, i, prev):
     if(i == len(arr)):
         return 0
-    excluded = lissubproblem(arr,i+1,prev)
+    excluded = UNOPTIMALlissubproblem(arr,i+1,prev)
     included = 0
     if(arr[i] > prev):
-        included = 1 + lissubproblem(arr, i+1, arr[i])
+        included = 1 + UNOPTIMALlissubproblem(arr, i+1, arr[i])
     return max(included,excluded)
 
-def lis(arr):
-    length = lissubproblem(arr, 0, -1)
+def UNOPTIMALlis(arr):
+    length = UNOPTIMALlissubproblem(arr, 0, -1)
     return length
 
-print(lis([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
-assert lis([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]) == 6, "Incorrect logic"
+print(UNOPTIMALlis([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
+assert UNOPTIMALlis([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]) == 6, "Incorrect logic"
